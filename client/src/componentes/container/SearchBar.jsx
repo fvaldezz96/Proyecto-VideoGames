28b28c7fb61cfbdb6b316598957ad98ae9f4d52e
Federico valdez <valdezfede21@gmail.com>
2022-07-09 12:45:37 -0300

    styleEnd2

--- client/src/componentes/container/SearchBar.jsx
+++ client/src/componentes/container/SearchBar.jsx
@@ -9,43 +9,32 @@ export default function SearchBar() {
    const dispatch = useDispatch();
    const [name, setName] = useState("");
 
-   /**
-    * La función handleChange toma un evento como argumento, evita la acción predeterminada y establece
-    * el estado del nombre en el valor del objetivo.
-    */
+
+   function handleSubmit(e) {
+      e.preventDefault();
+      dispatch(getNameVideogames(name));
+      // console.log(getNameVideogames())
+      setName("");
+   }
+
    function handleChange(e) {
       e.preventDefault();
       setName(e.target.value);
    }
 
-   function handleSubmit(e) {
-      e.preventDefault();
-      dispatch(getNameVideogames(name));
-      setName("");
-   }
-
-
    return (
-      <div>
-         <form
-            className="contenedorSearch"
-            onSubmit={(e) => handleSubmit(e)}
-         >
-            <div className="container-3">
-               <input
-                  type="search"
-                  id="search"
-                  value={name}
-                  onChange={(e) => handleChange(e)}
-                  placeholder="Buscar..."
-               />
-               <button className='botonIr' type='submit'><i class="fa-solid fa-magnifying-glass"></i></button>
-            </div>
-         </form>
+      <div className="contenedorSearch">
+         <div className="container-3">
+            <input
+               type="text"
+               id="search"
+               value={name}
+               onChange={(e) => handleChange(e)}
+               placeholder="Buscar..."
+            />
+            <button className='botonIr' type='submit' onClick={(e) => handleSubmit(e)}><i class="fa-solid fa-magnifying-glass"></i></button>
+         </div>
       </div>
    )
 }
-{/* <div class="container-3">
-   <span class="icon"><i class="fa fa-search"></i></span>
-   <input type="search" id="search" placeholder="Search..." />
-</div> */}
+
